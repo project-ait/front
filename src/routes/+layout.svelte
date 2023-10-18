2da4986768e7a4357c1cbedb287eb9b6f5c7bb40
Vbeo147 <vbeo147@gmail.com>
2023-10-18 15:10:42 +0900

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "./styles.css";
+</script>
+
+<div class="app">
+  <!-- Header 필요시 Header Component 추가 -->
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style>
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div>Hello World!</div>
+
+<style>
+</style>
