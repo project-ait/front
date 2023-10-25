a49fb26588c262d94f4697225e5b3be5a9832fbd
PleahMaCaka <pleahmacaka@gmail.com>
2023-10-25 10:04:07 +0900

    feat(chat): implement message

---
+++ src/lib/components/chat/Message.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  export let profilePic: string
+  export let bgColor: string
+  export let message: string
+</script>
+
+
+<div
+  class="chat-bg"
+  style="background-color: {bgColor};
+">
+  <img
+    class="chat-proc"
+    src={profilePic}
+    alt={"Profile"}
+  />
+  <p class="chat-msg">{message}</p>
+</div>
+
+<style>
+  .chat-bg {
+    display: flex;
+    flex-direction: row;
+
+    padding: 1rem;
+    border-radius: 1rem;
+    color: #f2f2f2;
+
+    margin: 0.5rem auto;
+    min-width: 320px;
+    max-width: 60%;
+
+    width: 100%;
+  }
+
+  .chat-proc {
+    width: 2.2rem;
+    height: 2.2rem;
+    border-radius: 50%;
+    margin-right: 1rem;
+
+    object-fit: cover;
+  }
+
+  .chat-msg {
+    font-weight: 400;
+    line-height: 1.5rem;
+    align-self: center;
+  }
+</style>
--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,8 +1,21 @@
 <script>
   import ChatInput from "$lib/components/share/ChatInput.svelte";
+  import Message from "$lib/components/chat/Message.svelte";
 </script>
 
 <div id="chat-page">
+  <div id="chat-history">
+    <Message
+      message="I hate facebook ads"
+      bgColor="rgba(0, 0, 0, 0.1)"
+      profilePic="guest.png"
+    />
+    <Message
+      message="Facebook is no more exists lol multiple lines multiple lines multiple lines multiple lines multiple lines multiple lines multiple lines multiple lines multiple lines multiple lines multiple lines multiple lines multiple lines multiple lines multiple lines multiple lines multiple lines"
+      bgColor="rgba(255, 255, 255, 0.05)"
+      profilePic="llama2.png"
+    />
+  </div>
   <ChatInput />
 </div>
 
@@ -16,5 +29,14 @@
     align-items: center;
 
     padding: 32px;
+  }
+
+  #chat-history {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+    overflow-y: auto;
+    margin-bottom: 2%;
   }
 </style>
