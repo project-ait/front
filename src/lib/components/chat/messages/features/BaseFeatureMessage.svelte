5bcb0fd1271b14958db1166914a2b594e8ccd274
PleahMaCaka <pleahmacaka@gmail.com>
2023-10-31 15:09:09 +0900

    feat: base feature component

---
+++ src/lib/components/chat/messages/features/BaseFeatureMessage.svelte
@@ -0,0 +1,18 @@
+<div class="feat-bg">
+  <slot />
+</div>
+
+<style>
+  .feat-bg {
+    background-color: #F7F7F7;
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+    color: #1F1F21;
+    border-radius: 10px;
+    min-width: 480px;
+
+    padding: 12px 16px;
+
+    border: 6px solid rgba(199, 191, 191, 0.86);
+    font-family: Cabin, serif;
+  }
+</style>
