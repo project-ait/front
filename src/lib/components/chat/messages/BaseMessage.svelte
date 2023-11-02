46fa0adeed9c10a44decdbc95faa87a571c13bcb
PleahMaCaka <pleahmacaka@gmail.com>
2023-11-02 14:21:45 +0900

    feat(BaseMessage): awesome slide-down animation

--- src/lib/components/chat/messages/BaseMessage.svelte
+++ src/lib/components/chat/messages/BaseMessage.svelte
@@ -37,6 +37,19 @@
     max-width: 35%;
 
     width: 100%;
+
+    animation: slideDown 0.3s ease-in-out;
+  }
+
+  @keyframes slideDown {
+    0% {
+      transform: translateY(-10px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0);
+      opacity: 1;
+    }
   }
 
   .chat-proc {
