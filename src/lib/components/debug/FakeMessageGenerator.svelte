8c09f45c49f8eba186fff6d1e36a6766a24a3da7
PleahMaCaka <pleahmacaka@gmail.com>
2023-11-02 14:09:45 +0900

    feat(F-M-G): features/clear button

--- src/lib/components/debug/FakeMessageGenerator.svelte
+++ src/lib/components/debug/FakeMessageGenerator.svelte
@@ -9,26 +9,47 @@
   const sendAs = async () => {
     await client.appendHistory(isBot ? Author.Assistant : Author.User, content)
   }
+
+  const sendWeather = async () => {
+    await client.appendHistory(Author.Assistant, " ` weather` Yes sir!")
+  }
+
+  const clearChat = async () => {
+    await client.clearHistory()
+  }
 </script>
 
 {#if $stateStore.debug}
   <div id="fmsg">
-    <div id="fmsg-is-this">
+    <div id="fake-msg-input">
       <h3>Is this a bot?</h3>
       <input type="checkbox" bind:value={isBot} />
     </div>
-    <div id="fmsg-is-this">
+    <div id="fake-msg-input">
       <input bind:value={content} />
       <button on:click={sendAs}>Send</button>
+    </div>
+    <div id="fake-msg-input">
+      <input type="button" value="Weather" on:click={sendWeather} />
+      <input type="button" value="Clear History" on:click={clearChat} />
     </div>
   </div>
 {/if}
 
 <style>
-  #fmsg-is-this {
+  #fake-msg-input {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+  }
+
+  #fake-msg-input input[type="button"] {
+    padding: 4px 8px;
+    margin: 4px 0;
+  }
+
+  #fake-msg-input input {
+    padding: 2px 2px;
   }
 
   #fmsg {
